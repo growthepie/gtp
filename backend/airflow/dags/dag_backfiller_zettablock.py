08e82b7f02c1fff1619166deba689e6ff2b7ea6c
MSeidl93 <matthias.seidl@gmx.de>
2024-01-31 10:14:13 +0000

    handled no missing range

--- backend/airflow/dags/dag_backfiller_zettablock.py
+++ backend/airflow/dags/dag_backfiller_zettablock.py
@@ -43,25 +43,28 @@ def etl():
         end_block = db_connector.get_min_block(tbl, end_date)
 
         missing_block_ranges = check_and_record_missing_block_ranges(db_connector, tbl, start_block, end_block)
+        ## if missing_blocks_ranges is type boolean and False, then no missing blocks were found
+        if not missing_block_ranges:
+            print(f"No missing blocks found for table: {tbl}.")
+        else:
+            adapter_params = {
+                'api_key' : os.getenv("ZETTABLOCK_API_2")
+            }
 
-        adapter_params = {
-            'api_key' : os.getenv("ZETTABLOCK_API_2")
-        }
+            ad = AdapterZettaBlockRaw(adapter_params, db_connector)
 
-        ad = AdapterZettaBlockRaw(adapter_params, db_connector)
+            for i, block_range in enumerate(missing_block_ranges):
+                if block_range[1] - block_range[0] > 1: ## more than 1 empty block
+                    print(f'{i}/{len(missing_block_ranges)}: extracting block range {block_range}')
+                    load_params = {
+                        'keys' : [tbl],
+                        'block_start' : block_range[0], ## block number as int
+                        'block_end' : block_range[1], ## block number as int
+                        'step_overwrite' : True
+                    }
 
-        for i, block_range in enumerate(missing_block_ranges):
-            if block_range[1] - block_range[0] > 1: ## more than 1 empty block
-                print(f'{i}/{len(missing_block_ranges)}: extracting block range {block_range}')
-                load_params = {
-                    'keys' : [tbl],
-                    'block_start' : block_range[0], ## block number as int
-                    'block_end' : block_range[1], ## block number as int
-                    'step_overwrite' : True
-                }
-
-                # extract & load incremmentally
-                ad.extract_raw(load_params)
+                    # extract & load incremmentally
+                    ad.extract_raw(load_params)
 
     @task()
     def backfiller_polygon_zkevm():
@@ -78,24 +81,28 @@ def etl():
 
         missing_block_ranges = check_and_record_missing_block_ranges(db_connector, tbl, start_block, end_block)
 
-        adapter_params = {
-            'api_key' : os.getenv("ZETTABLOCK_API_2")
-        }
+        ## if missing_blocks_ranges is type boolean and False, then no missing blocks were found
+        if not missing_block_ranges:
+            print(f"No missing blocks found for table: {tbl}.")
+        else:
+            adapter_params = {
+                'api_key' : os.getenv("ZETTABLOCK_API_2")
+            }
 
-        ad = AdapterZettaBlockRaw(adapter_params, db_connector)
+            ad = AdapterZettaBlockRaw(adapter_params, db_connector)
 
-        for i, block_range in enumerate(missing_block_ranges):
-            if block_range[1] - block_range[0] > 1: ## more than 1 empty block
-                print(f'{i}/{len(missing_block_ranges)}: extracting block range {block_range}')
-                load_params = {
-                    'keys' : [tbl],
-                    'block_start' : block_range[0], ## block number as int
-                    'block_end' : block_range[1], ## block number as int
-                    'step_overwrite' : True
-                }
+            for i, block_range in enumerate(missing_block_ranges):
+                if block_range[1] - block_range[0] > 1: ## more than 1 empty block
+                    print(f'{i}/{len(missing_block_ranges)}: extracting block range {block_range}')
+                    load_params = {
+                        'keys' : [tbl],
+                        'block_start' : block_range[0], ## block number as int
+                        'block_end' : block_range[1], ## block number as int
+                        'step_overwrite' : True
+                    }
 
-                # extract & load incremmentally
-                ad.extract_raw(load_params)
+                    # extract & load incremmentally
+                    ad.extract_raw(load_params)
 
     backfiller_zksync_era()
     backfiller_polygon_zkevm()
