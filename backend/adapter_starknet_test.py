615eef27ed468bc0fe37ef1d7179bbd3e6afcbeb
naderbennour <naderbennour@outlook.com>
2024-02-02 13:57:41 +0100

    added starknet adapter and dag

---
+++ backend/adapter_starknet_test.py
@@ -0,0 +1,27 @@
+import os
+from src.adapters.starknet_adapter import StarkNetAdapter
+from src.db_connector import DbConnector
+
+adapter_params = {
+    'chain': 'starknet',
+    'rpc_url': os.getenv("STARKNET_RPC"),
+}
+
+# Initialize DbConnector
+db_connector = DbConnector()
+
+# Initialize StarkNetAdapter
+adapter = StarkNetAdapter(adapter_params, db_connector)
+
+# Test run method
+load_params = {
+    'block_start': 'auto',
+    'batch_size': 10,
+    'threads': 10,
+}
+
+try:
+    adapter.extract_raw(load_params)
+except Exception as e:
+    print(f"Extraction stopped due to an exception: {e}")
+    raise e
