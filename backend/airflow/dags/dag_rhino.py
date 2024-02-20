d496443e06fd12c83f279b64e1dd549251c262fa
MSeidl93 <matthias.seidl@gmx.de>
2024-02-20 14:58:49 +0100

    fixed path

--- backend/airflow/dags/dag_rhino.py
+++ backend/airflow/dags/dag_rhino.py
@@ -6,7 +6,7 @@ sys.path.append(f"/home/{sys_user}/gtp/backend/")
 import os
 from datetime import datetime, timedelta
 from airflow.decorators import dag, task
-from backend.src.adapters.adapter_rhino import AdapterRhino
+from src.adapters.adapter_rhino import AdapterRhino
 from src.db_connector import DbConnector
 from src.adapters.adapter_utils import *
 
