3f6571caca7bb4da1852c3918a0f5737f46b14b8
MSeidl93 <matthias.seidl@gmx.de>
2024-03-01 18:10:18 +0100

    added screenshot functionality

--- backend/airflow/dags/dag_screenshot_to_s3.py
+++ backend/airflow/dags/dag_screenshot_to_s3.py
@@ -5,18 +5,9 @@ sys_user = getpass.getuser()
 import sys
 sys.path.append(f"/home/{sys_user}/gtp/backend/")
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from PIL import Image
-import time
-import os
-import json
-
+from src.api.screenshots_to_s3 import run_screenshots
 from airflow.decorators import dag, task 
-
-FUND_CAPTURE = [1320, 350, 2075, 770]
-CHAIN_CAPTURE = [765, 245, 2070, 600] 
-BLOCK_CAPTURE = [1320, 350, 2075, 770]
+import os
 
 default_args = {
     'owner' : 'mseidl',
@@ -31,63 +22,14 @@ default_args = {
     dag_id = 'dag_screenshot_loader',
     description = 'Create and store screenshots in s3 bucket',
     start_date = datetime(2023,4,24),
-    schedule = '02 02 * * *'
+    schedule = '00 06 * * *'
 )
 
 def etl():
     @task()
-    def capture_screenshot(url, output_path, coords):
-        options = Options()
-        options.add_argument('--headless') 
-        options.add_argument('--disable-gpu') 
-
-        driver = webdriver.Chrome(options=options)
-
-        try:
-            driver.set_window_size(2560, 1440)
-            driver.get(url)
-            time.sleep(3)
-            #Sleep allows page load.
-            driver.save_screenshot(output_path)
-                    
-            im = Image.open(output_path)
-            im_cropped = im.crop(coords)
-            im_cropped.save(output_path)
-        finally:
-            driver.quit()
-            
-            
-    def run_screenshots():
-        if not os.path.exists("../Screenshots"):
-            os.makedirs("../Screenshots")
-            os.makedirs("../Screenshots/Fundamentals")
-            os.makedirs("../Screenshots/Chains")
-            os.makedirs("../Screenshots/Blockspace")
-        with open('../../src/misc/navigation_data.json', 'r') as file:
-            data = json.load(file)
-        #Generate folders for image if not existing
-        
-        for i in data['Fundamentals']["options"]:
-            url = "https://www.growthepie.xyz/fundamentals/"
-            url = url + "" + i["urlKey"]
-            path = "Screenshots/" + "Fundamentals/" + i["urlKey"] + ".png"
-            capture_screenshot(url, path, i["coords"] if "coords" in i else FUND_CAPTURE)
-
-            
-        for i in data['Chains']["options"]:
-            url = "https://www.growthepie.xyz/chains/"
-            url = url + "" + i["urlKey"]
-            path = "Screenshots/" + "Chains/" + i["urlKey"] + ".png"
-            capture_screenshot(url, path, i["coords"] if "coords" in i else CHAIN_CAPTURE)
-            
-        for i in data['Blockspace']["options"]:
-            url = "https://www.growthepie.xyz/blockspace/"
-            url = url + "" + i["urlKey"]
-            path = "Screenshots/" + "Blockspace/" + i["urlKey"] + ".png"
-            capture_screenshot(url, path, i["coords"] if "coords" in i else BLOCK_CAPTURE)
-        #Capture screenshot loop for each image group
+    def run_screenshots_task():
+        run_screenshots(os.getenv("S3_CF_BUCKET"), os.getenv("CF_DISTRIBUTION_ID"), 'v1')
     
-    run_screenshots()
-
+    run_screenshots_task()
 etl()
 
---
+++ backend/src/api/screenshots_to_s3.py
@@ -0,0 +1,179 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from src.misc.helper_functions import upload_png_to_cf_s3
+from PIL import Image
+import time
+import os
+
+FUND_CAPTURE = [1320, 350, 2075, 770]
+CHAIN_CAPTURE = [765, 245, 2070, 600] 
+BLOCK_CAPTURE = [1320, 350, 2075, 770]
+
+def capture_screenshot(url, output_path, coords):
+    options = Options()
+    options.add_argument('--headless') 
+    options.add_argument('--disable-gpu') 
+
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.set_window_size(2560, 1440)
+        driver.get(url)
+        time.sleep(3)
+        #Sleep allows page load.
+        driver.save_screenshot(output_path)
+                
+        im = Image.open(output_path)
+        im_cropped = im.crop(coords)
+        im_cropped.save(output_path)
+
+        return im_cropped
+    finally:
+        driver.quit()
+
+def run_screenshots(s3_bucket, cf_distribution_id, api_version):
+    main_path = '../backend/src/api/screenshots'
+    main_url = 'https://www.growthepie.xyz'
+
+    #Generate folders for image if not existing
+    if not os.path.exists(main_path):
+        os.makedirs(main_path)
+        os.makedirs(f"{main_path}/fundamentals")
+        os.makedirs(f"{main_path}/chains")
+        os.makedirs(f"{main_path}/blockspace")
+ 
+    for key in screenshot_data:
+        label = key.lower()
+        for i in screenshot_data[key]["options"]:
+            url = f"{main_url}/{label}/{i['urlKey']}"
+            path = f"{main_path}/{label}/{i['urlKey']}.png"
+            capture_screenshot(url, path, i["coords"] if "coords" in i else FUND_CAPTURE)
+            
+            s3_path = f'{api_version}/og_images/{label}/{i["urlKey"]}'
+            upload_png_to_cf_s3(s3_bucket, s3_path, path, cf_distribution_id)
+
+        
+## This is the data structure for the screenshot data
+screenshot_data = {
+    "Fundamentals": {
+      "label": "Fundamentals",
+      "key": "metrics",
+      "options": [
+        {
+          "label": "Daily Active Addresses",
+          "urlKey": "daily-active-addresses"
+        },
+        {
+          "label": "Transaction Count",
+          "urlKey": "transaction-count"
+        },
+        {
+          "label": "Stablecoin Market Cap",
+          "urlKey": "stablecoin-market-cap"
+        },
+        {
+          "label": "Total Value Locked",
+          "urlKey": "total-value-locked"
+        },
+        {
+          "label": "Fees Paid by Users",
+          "urlKey": "fees-paid-by-users"
+        },
+        {
+          "label": "Rent Paid to L1",
+          "urlKey": "rent-paid"
+        },
+        {
+          "label": "Onchain Profit",
+          "urlKey": "profit"
+        },
+        {
+          "label": "Fully Diluted Valuation",
+          "urlKey": "fdv",
+          "coords": [1320, 370, 2075, 790]
+        },
+        {
+          "label": "Market Cap",
+          "urlKey": "market-cap",
+          "coords": [1320, 370, 2075, 790]
+        },
+        
+        {
+          "label": "Transaction Costs",
+          "urlKey": "transaction-costs"
+        }
+      ]
+    },
+    "Blockspace": {
+        "label": "Blockspace", 
+        "options": [
+          {
+            "label": "Chain Overview",
+            "urlKey": "chain-overview",
+            "coords" : [760, 310, 2075, 1030] 
+          },
+          {
+            "label": "Category Comparison",
+            "urlKey": "category-comparison",
+            "coords" : [765, 315, 2070, 1020] 
+          }
+        ]
+      },
+      "Chains": {
+        "label": "Single Chain",
+        "key": "chains",
+        "options": [
+          {
+            "label": "Ethereum",
+            "urlKey": "ethereum",
+            "coords": [765, 320, 2070, 705] 
+          },
+          {
+            "label": "Base",
+            "urlKey": "base",
+            "coords": [765, 290, 2070, 680] 
+          },
+          {
+            "label": "OP Mainnet",
+            "urlKey": "optimism",
+            "coords": [765, 340, 2070, 730] 
+          },
+          {
+            "label": "Public Goods Network",
+            "urlKey": "public-goods-network",
+            "coords": [765, 290, 2070, 680] 
+          },
+          {
+            "label": "Zora",
+            "urlKey": "zora",
+            "coords": [765, 290, 2070, 680] 
+          },
+          {
+            "label": "Arbitrum",
+            "urlKey": "arbitrum",
+            "coords": [765, 315, 2070, 710] 
+          },
+          {
+            "label": "Polygon zkEVM",
+            "urlKey": "polygon-zkevm",
+            "coords": [765, 340, 2070, 730]
+          },
+          {
+            "label": "zkSync Era",
+            "urlKey": "zksync-era",
+            "coords": [765, 320, 2070, 705] 
+          },
+          {
+            "label": "Linea",
+            "urlKey": "linea",
+            "coords": [765, 320, 2070, 705] 
+          },
+          {
+            "label": "Immutable X",
+            "urlKey": "immutable-x",
+            "coords": [765, 320, 2070, 705] 
+          }
+        ]
+      }
+  }
