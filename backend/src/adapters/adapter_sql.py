9d11e8cba0f03598935e5d9836106dca9e1f89ba
MSeidl93 <matthias.seidl@gmx.de>
2024-03-04 10:53:24 +0100

    added aa_last7d limitation

--- backend/src/adapters/adapter_sql.py
+++ backend/src/adapters/adapter_sql.py
@@ -130,6 +130,8 @@ class AdapterSQL(AbstractAdapter):
                     day_val = 7
                 elif query.metric_key == 'aa_last30d':
                     day_val = 3
+                elif query.metric_key == 'aa_last7d':
+                    day_val = 3
                 else:
                     day_val = get_missing_days_kpis(self.db_connector, metric_key= query.metric_key, origin_key=query.origin_key)
             else:
