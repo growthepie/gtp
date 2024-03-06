6000831385c2be50dc8da1c6cf09a93ac523a751
MSeidl93 <matthias.seidl@gmx.de>
2024-03-06 16:20:33 +0100

    lowered threads

--- backend/airflow/dags/raw/raw_base.py
+++ backend/airflow/dags/raw/raw_base.py
@@ -43,7 +43,7 @@ def adapter_rpc():
         # Initial load parameters
         load_params = {
             'block_start': 'auto',
-            'batch_size': 100,
+            'batch_size': 50,
             'threads': 3,
         }
 
