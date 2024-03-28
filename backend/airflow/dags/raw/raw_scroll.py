0fbe2f123cc1ded42373d472fdb625c5cbede1f3
MSeidl93 <matthias.seidl@gmx.de>
2024-03-28 11:18:54 +0100

    5 threads, 20 batch

--- backend/airflow/dags/raw/raw_scroll.py
+++ backend/airflow/dags/raw/raw_scroll.py
@@ -49,8 +49,8 @@ def adapter_rpc():
         # Initial load parameters
         load_params = {
             'block_start': 'auto',
-            'batch_size': 10,
-            'threads': 3,
+            'batch_size': 20,
+            'threads': 5,
         }
 
         while load_params['threads'] > 0:
