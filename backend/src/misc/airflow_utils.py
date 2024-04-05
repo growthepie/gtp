3ff1162d3ea5b9ee7d836c0c111894ab526e3b5d
MSeidl93 <matthias.seidl@gmx.de>
2024-04-05 15:23:38 +0200

    use Airflow variable

--- backend/src/misc/airflow_utils.py
+++ backend/src/misc/airflow_utils.py
@@ -1,11 +1,12 @@
 import os
 from src.misc.helper_functions import send_discord_message
+from airflow.models import Variable
 
 def alert_via_webhook(context):
     dag_run = context.get('dag_run')
     task_instance = context.get('task_instance')
     exception = context.get('exception')
-    webhook_url = os.getenv("DISCORD_ALERTS")
+    webhook_url = Variable.get("DISCORD_ALERTS")
 
     message = f"Alert: A failure occurred in {dag_run.dag_id} on task {task_instance.task_id}. Exception: {exception}"
     send_discord_message(message, webhook_url)
