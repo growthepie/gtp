9916773bcfaf74e6ea8b7dd94ee57ee623c55931
MSeidl93 <matthias.seidl@gmx.de>
2024-04-05 15:12:58 +0200

    moved airflow utils

--- backend/airflow/dags/dummy.py
+++ backend/airflow/dags/dummy.py
@@ -1,13 +1,12 @@
 from datetime import datetime,timedelta
 import getpass
-import os
 sys_user = getpass.getuser()
 
 import sys
 sys.path.append(f"/home/{sys_user}/gtp/backend/")
 from airflow.decorators import dag, task 
 
-from airflow.airflow_utils import alert_via_webhook
+from src.misc.airflow_utils import alert_via_webhook
 
 @dag(
     default_args={
