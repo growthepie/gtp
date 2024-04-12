ef677eb0535701b66fe4e42b707c56641cb3223f
MSeidl93 <matthias.seidl@gmx.de>
2024-04-12 10:17:44 +0200

    remove Mode from API output

--- backend/src/chain_config.py
+++ backend/src/chain_config.py
@@ -708,7 +708,7 @@ adapter_mapping = [
         ,da_layer = "Ethereum (blobs or calldata)"
         ,rhino_naming='MODE'
 
-        ,in_api = True
+        ,in_api = False
         ,in_fees_api = True
         ,deployment="DEV"
         ,exclude_metrics = ['blockspace', 'fdv', 'market_cap']
