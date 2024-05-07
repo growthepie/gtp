6b3756447f28132a95b64fd037417d9e95f8a496
MSeidl93 <matthias.seidl@gmx.de>
2024-05-07 14:42:09 +0200

    batch_size and backfiller_on optional, added blast, removed required chains

--- backend/airflow/dags/backfill/backfill_rpc_new.py
+++ backend/airflow/dags/backfill/backfill_rpc_new.py
@@ -17,14 +17,12 @@ from src.chain_config import adapter_mapping
 # config: Environment variable containing the RPC node configuration
 # backfiller_on: Whether the chain is backfiller_on and should be backfilled
 
-required_chains = ['blast', 'ethereum', 'zksync_era', 'base', 'mantle', 'arbitrum']
-
 chain_settings = {
     adapter.origin_key: {
         'batch_size': adapter.batch_size, 
         'backfiller_on': adapter.backfiller_on
     }
-    for adapter in adapter_mapping if adapter.origin_key in required_chains
+    for adapter in adapter_mapping if adapter.backfiller_on == True
 }
 
 @dag(
@@ -43,45 +41,44 @@ chain_settings = {
 )
 def backfiller_dag():
     for chain, settings in chain_settings.items():
-        if settings['backfiller_on']:
-            @task(task_id=f'new_backfill_{chain}')
-            def run_backfill_task(chain_name, db_connector, start_date, end_date, batch_size):
-                rpc_configs = get_chain_config(db_connector, chain_name)
-                w3 = None
+        @task(task_id=f'new_backfill_{chain}')
+        def run_backfill_task(chain_name, db_connector, start_date, end_date, batch_size):
+            rpc_configs = get_chain_config(db_connector, chain_name)
+            w3 = None
 
-                for rpc_config in rpc_configs:
-                    try:
-                        w3 = Web3CC(rpc_config)
-                        break
-                    except Exception as e:
-                        print(f"Failed to connect to RPC URL: {rpc_config['url']} with error: {e}")
-
-                if not w3:
-                    raise ConnectionError("Failed to connect to any provided RPC node.")
-
-                start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
-                end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
-                start_timestamp = date_to_unix_timestamp(start_date_obj.year, start_date_obj.month, start_date_obj.day)
-                end_timestamp = date_to_unix_timestamp(end_date_obj.year, end_date_obj.month, end_date_obj.day)
-                start_block = find_first_block_of_day(w3, start_timestamp)
-                end_block = find_last_block_of_day(w3, end_timestamp)
-
-                adapter_params = {'chain': chain_name, 'rpc_configs': rpc_configs}
-                node_adapter = NodeAdapter(adapter_params, db_connector)
-
+            for rpc_config in rpc_configs:
                 try:
-                    node_adapter.backfill_missing_blocks(start_block, end_block, batch_size)
-                    print("Backfill process completed successfully.")
+                    w3 = Web3CC(rpc_config)
+                    break
                 except Exception as e:
-                    print(f"Backfiller: An error occurred: {e}", file=sys.stderr)
-                    raise
+                    print(f"Failed to connect to RPC URL: {rpc_config['url']} with error: {e}")
 
-            batch_size = settings['batch_size']
-            db_connector = DbConnector()
+            if not w3:
+                raise ConnectionError("Failed to connect to any provided RPC node.")
 
-            start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
-            end_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+            start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
+            start_timestamp = date_to_unix_timestamp(start_date_obj.year, start_date_obj.month, start_date_obj.day)
+            end_timestamp = date_to_unix_timestamp(end_date_obj.year, end_date_obj.month, end_date_obj.day)
+            start_block = find_first_block_of_day(w3, start_timestamp)
+            end_block = find_last_block_of_day(w3, end_timestamp)
 
-            run_backfill_task(chain_name=chain, db_connector=db_connector, start_date=start_date, end_date=end_date, batch_size=batch_size)
+            adapter_params = {'chain': chain_name, 'rpc_configs': rpc_configs}
+            node_adapter = NodeAdapter(adapter_params, db_connector)
+
+            try:
+                node_adapter.backfill_missing_blocks(start_block, end_block, batch_size)
+                print("Backfill process completed successfully.")
+            except Exception as e:
+                print(f"Backfiller: An error occurred: {e}", file=sys.stderr)
+                raise
+
+        batch_size = settings['batch_size']
+        db_connector = DbConnector()
+
+        start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+        end_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+        run_backfill_task(chain_name=chain, db_connector=db_connector, start_date=start_date, end_date=end_date, batch_size=batch_size)
 
 backfiller_dag_instance = backfiller_dag()
