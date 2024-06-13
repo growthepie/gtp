eaf0bb47bac728d31bed51f7c80b63303efc4987
mseidlx <matthias@mseidl-analytics.de>
2024-06-13 12:17:43 +0200

    removed old backfiller, optimism on new backfiller

--- backend/src/adapters/funcs_backfill.py
+++ backend/src/adapters/funcs_backfill.py
@@ -83,36 +83,6 @@ def check_and_record_missing_block_ranges(db_connector, table_name, start_block,
     # print(f"Missing block ranges saved to {missing_blocks_file}")
     return missing_ranges
 
-def process_missing_blocks_in_batches(db_connector, s3_connection, missing_block_ranges, batch_size, threads, chain_name, table_name, w3):
-    # with open(json_file, 'r') as file:
-    #     missing_block_ranges = json.load(file)
-
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        futures = []
-
-        for block_range in missing_block_ranges:
-            start_block = block_range[0]
-            end_block = block_range[1]
-
-            for batch_start in range(start_block, end_block + 1, batch_size):
-                batch_end = min(batch_start + batch_size - 1, end_block)
-
-                future = executor.submit(fetch_and_process_range, batch_start, batch_end, chain_name, w3, table_name, s3_connection, BUCKET_NAME, db_connector)
-                futures.append(future)
-
-        for future in as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(f"An error occurred: {e}")
-
-    # # After processing all ranges, delete the JSON file
-    # try:
-    #     os.remove(json_file)
-    #     print(f"Successfully deleted the file: {json_file}")
-    # except OSError as e:
-    #     print(f"Error: {e.filename} - {e.strerror}.")
-
 def date_to_unix_timestamp(year, month, day):
     return int(time.mktime(datetime(year, month, day).timetuple()))
 
@@ -156,48 +126,3 @@ def find_last_block_of_day(w3, target_timestamp):
             return mid_block
 
     return max_block  # The last block that is still within the day
-
-def backfiller_task(chain_name, start_date, end_date, threads, batch_size):
-    # Initialize DbConnector
-    db_connector = DbConnector()   
-    
-    s3_connection, _ = connect_to_s3()
-
-    # Dynamic RPC URL based on chain name
-    rpc_url = os.getenv(chain_name.upper() + "_RPC")
-
-    # Connect to node
-    w3 = connect_to_node(rpc_url)
-    if w3 is None:
-        print("Failed to connect to the Ethereum node.")
-        sys.exit(1)
-
-    # Convert start_date and end_date from strings to datetime objects
-    start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
-
-    # Convert dates to timestamps
-    start_timestamp = date_to_unix_timestamp(start_date_obj.year, start_date_obj.month, start_date_obj.day)
-    end_timestamp = date_to_unix_timestamp(end_date_obj.year, end_date_obj.month, end_date_obj.day)
-
-    # Determine the block number range based on dates
-    start_block = find_first_block_of_day(w3, start_timestamp)
-    end_block = find_last_block_of_day(w3, end_timestamp)
-
-    # Dynamic chain-based table name
-    table_name = chain_name + "_tx"
-
-    # # Dynamic file name based on chain name
-    # missing_blocks_file = f"missing_blocks_{table_name}.json"
-
-    print(f"Checking blocks from {start_block} to {end_block} in {table_name}")
-    
-    # Check and record missing block ranges
-    missing_blocks_ranges = check_and_record_missing_block_ranges(db_connector, table_name, start_block, end_block)
-
-    ## if missing_blocks_ranges is type boolean and False, then no missing blocks were found
-    if not missing_blocks_ranges:
-        print(f"No missing blocks found for table: {table_name}.")
-    else:
-        # Process missing blocks in batches 
-        process_missing_blocks_in_batches(db_connector, s3_connection, missing_blocks_ranges, batch_size, threads, chain_name, table_name, w3)
