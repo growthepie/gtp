203a2f7ea54624832f7dee20dac1e24ba7330503
mseidlx <matthias@mseidl-analytics.de>
2024-06-15 10:30:53 +0200

    fixed missing columns

--- backend/src/misc/airtable_functions.py
+++ backend/src/misc/airtable_functions.py
@@ -68,6 +68,10 @@ def read_all_labeled_contracts_airtable(table):
         df['contract_name'] = ''
     if 'owner_project' not in df.columns:
         df['owner_project'] = ''
+    if 'owner_project_lookup' not in df.columns:
+        df['owner_project_lookup'] = ''
+    if 'usage_category_lookup' not in df.columns:
+        df['usage_category_lookup'] = ''
     if 'labelling_type' not in df.columns:
         df['labelling_type'] = ''
 
