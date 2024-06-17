4f4a0c33269f4fba777b55d97496462fc9f904fa
mseidlx <matthias@mseidl-analytics.de>
2024-06-17 10:42:32 +0200

    conf: include Metis in fees API

--- backend/src/chain_config.py
+++ backend/src/chain_config.py
@@ -673,7 +673,7 @@ adapter_mapping = [
         ,da_layer = "MEMO"
 
         ,in_api = True
-        ,in_fees_api = False
+        ,in_fees_api = True
         ,deployment="DEV"
         ,exclude_metrics = ['blockspace', 'profit', 'rent_paid']
         ,aggregate_blockspace = True
