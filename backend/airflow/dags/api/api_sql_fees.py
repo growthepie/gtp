4c78f1151a9646134c31ecdaf98e2c83d0673f22
mseidlx <matthias@mseidl-analytics.de>
2024-06-29 14:38:58 +0200

    parrallelized the fees generation

--- backend/airflow/dags/api/api_sql_fees.py
+++ backend/airflow/dags/api/api_sql_fees.py
@@ -13,6 +13,7 @@ import pandas as pd
 import os
 from src.adapters.adapter_sql import AdapterSQL
 from src.api.json_creation import JSONCreation
+from src.chain_config import adapter_mapping
 
 @dag(
     default_args={
@@ -29,29 +30,39 @@ from src.api.json_creation import JSONCreation
     schedule_interval='10,40 * * * *'
 )
 
-def etl():
+def create_aggregate_metrics_task(origin_key):
+    @task(task_id=f'agg_fees_metrics_{origin_key}')
+    def run_aggregate_metrics():
+        adapter_params = {}
+
+        load_params = {
+            'load_type': 'fees',
+            'days': 1,
+            'origin_keys': [origin_key],
+        }
+
+        # initialize adapter
+        db_connector = DbConnector()
+        ad = AdapterSQL(adapter_params, db_connector)
+        # extract
+        ad.extract(load_params)
+    
+    return run_aggregate_metrics
+
+def fees_json_gen_dag():
         @task()
-        def run_aggregate_metrics():
-                adapter_params = {}
-
-                load_params = {
-                        'load_type' : 'fees',
-                        'days' : 1,
-                }
-
-                # initialize adapter
-                db_connector = DbConnector()
-                ad = AdapterSQL(adapter_params, db_connector)
-                # extract
-                ad.extract(load_params)
-
-        @task()
-        def run_create_fees_json(run_aggregate_metrics:str):
+        def run_create_fees_json():
                 db_connector = DbConnector()
                 json_creator = JSONCreation(os.getenv("S3_CF_BUCKET"), os.getenv("CF_DISTRIBUTION_ID"), db_connector, "v1")
                 df = json_creator.get_data_fees()
                 json_creator.create_fees_table_json(df)
                 json_creator.create_fees_linechart_json(df)
+
+        aggregate_metrics_tasks = [
+            create_aggregate_metrics_task(chain.origin_key)()
+            for chain in adapter_mapping if chain.in_fees_api
+        ]
    
-        run_create_fees_json(run_aggregate_metrics())
-etl()
+        run_create_fees_json().set_upstream(aggregate_metrics_tasks)
+
+fees_json_gen_dag()
