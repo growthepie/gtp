706d8c890ea5a5fc272880c4ee1143aa864e95d5
mseidlx <matthias@mseidl-analytics.de>
2024-07-11 10:42:45 +0200

    added manta celestia blob size query

--- backend/src/queries/sql_queries.py
+++ backend/src/queries/sql_queries.py
@@ -1523,6 +1523,21 @@ sql_q= {
         GROUP BY 1
         """
 
+        , 'manta_celestia_blob_size_bytes': """
+        select 
+                date_trunc('day', block_timestamp) as day, 
+                sum(blob_sizes) as value
+        from (
+                SELECT tx_hash, block_timestamp, jsonb_array_elements(blob_sizes::jsonb)::numeric as blob_sizes , jsonb_array_elements(namespaces::jsonb)::text AS namespaces 
+                FROM celestia_tx
+                where sender in (SELECT json_array_elements_text(da_mapping->'celestia') FROM sys_chains where origin_key = 'manta')
+                        and block_timestamp > date_trunc('day', now()) - interval '{{Days}} days'
+                        AND block_timestamp < date_trunc('day', now())
+        ) a
+        group by 1
+
+        """
+
         ### Mode
         ,'mode_txcount_raw': """
         SELECT date_trunc('day', gpt.block_timestamp) AS day,
@@ -1948,6 +1963,7 @@ sql_queries = [
         ,SQLQuery(metric_key = "txcosts_median_eth", origin_key = "manta", sql=sql_q["manta_txcosts_median_eth"], query_parameters={"Days": 7})
         ,SQLQuery(metric_key = "cca", origin_key = "manta", sql=get_cross_chain_activity('manta'), currency_dependent = False, query_parameters={})
         ,SQLQuery(metric_key = "gas_per_second", origin_key = "manta", sql=sql_q["manta_gas_per_second"], currency_dependent = False, query_parameters={"Days": 7})
+        ,SQLQuery(metric_key = "celestia_blob_size_bytes", origin_key = "manta", sql=sql_q["manta_celestia_blob_size_bytes"], currency_dependent = False, query_parameters={"Days": 7})
 
         ## Mode
         ,SQLQuery(metric_key = "txcount_raw", origin_key = "mode", sql=sql_q["mode_txcount_raw"], currency_dependent = False, query_parameters={"Days": 30})
