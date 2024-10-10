f993c0dbacfde14bbb9810e7551c55881709745a
naderbennour <naderbennour@outlook.com>
2024-10-10 11:38:39 +0200

    moved linea , scroll and zksync_era

--- backend/src/adapters/adapter_raw_rpc.py
+++ backend/src/adapters/adapter_raw_rpc.py
@@ -196,13 +196,14 @@ class NodeAdapter(AbstractAdapterRaw):
         arbitrum_nitro_chains = ['arbitrum', 'gravity']
         op_chains = ['zora', 'base', 'optimism', 'gitcoin_pgn', 'mantle', 'mode', 'blast', 'redstone', 'orderly', 'derive', 'karak', 'ancient8', 'kroma', 'fraxtal', 'cyber']
         default_chains = ['manta', 'metis']
+        special_chains = ['linea', 'scroll', 'zksync_era']
         while rpc_config['url'] in self.active_rpcs and not block_range_queue.empty():
             block_range = None
             try:
                 block_range = block_range_queue.get(timeout=5)
                 print(f"...processing block range {block_range[0]}-{block_range[1]} from {rpc_config['url']}")
         
-                if self.chain.lower() in op_chains or self.chain.lower() in arbitrum_nitro_chains or self.chain.lower() in default_chains:
+                if self.chain.lower() in op_chains or self.chain.lower() in arbitrum_nitro_chains or self.chain.lower() in default_chains or self.chain.lower() in special_chains:
                     print("NEW")
                     fetch_and_process_range2(block_range[0], block_range[1], self.chain, node_connection, self.table_name, self.s3_connection, self.bucket_name, self.db_connector, rpc_config['url'])
                 else:
