04b7aa4b86413f3feb6db0c0b8cb7895f0457707
mseidlx <matthias@mseidl-analytics.de>
2024-10-10 11:42:31 +0200

    octant_v2 on DAG

--- backend/airflow/dags/other/other_octant.py
+++ backend/airflow/dags/other/other_octant.py
@@ -1,19 +1,21 @@
 from datetime import datetime,timedelta
 import getpass
+import dotenv
 sys_user = getpass.getuser()
 
 import sys
 sys.path.append(f"/home/{sys_user}/gtp/backend/")
 
+import os
 from airflow.decorators import dag, task 
 from src.misc.airflow_utils import alert_via_webhook
 from src.db_connector import DbConnector
-from src.misc.octant import Octant
+from src.misc.octant_v2 import OctantV2
 
 @dag(
     default_args={
         'owner' : 'mseidl',
-        'retries' : 10,
+        'retries' : 5,
         'email_on_failure': False,
         'retry_delay' : timedelta(minutes=2),
         'on_failure_callback': alert_via_webhook
@@ -22,35 +24,20 @@ from src.misc.octant import Octant
     description='Load data from Octant API for tracker.',
     tags=['other', 'near-real-time'],
     start_date=datetime(2024,7,22),
-    schedule='*/2 * * * *'
+    schedule='*/30 * * * *'
 )
 
 def run_dag():
     @task()
-    def run_data_export():
-        db_connector = DbConnector(db_name='fun')
+    def run_octant_v2():      
+        dotenv.load_dotenv()
+        api_version = "v1"
+        db_connector = DbConnector(db_name="fun")
 
-        oct = Octant()
-        last_epoch = oct.get_last_epoch()
-        start_end_times = oct.get_epoch_start_end_times(last_epoch)
-        epoch_data, checksum = oct.get_epochs_data(start_end_times)
-
-        exec_string = """
-            SELECT checksum
-            FROM octant_rounds
-            ORDER BY id DESC
-            LIMIT 1;
-        """
-        db_checksum = db_connector.engine.execute(exec_string).scalar()
-
-        if db_checksum == checksum:
-            print('No new data')
-        else:
-            print('New data, inserting into db')   
-            exec_string = f"""
-                INSERT INTO octant_rounds (value, checksum, created_at)
-                VALUES ('{epoch_data}', '{checksum}', NOW());
-            """
-            db_connector.engine.execute(exec_string)
-    run_data_export()
+        octantv2 = OctantV2(os.getenv("S3_CF_BUCKET"), os.getenv("CF_DISTRIBUTION_ID"), db_connector, api_version)
+        #octantv2.run_load_octant_data_for_all_epochs()
+        octantv2.run_load_latest_epoch_data()
+        octantv2.run_create_all_octant_jsons()
+    
+    run_octant_v2()
 run_dag()
