473d088bf603bc496471f8996572289e64f30ba7
lorenz234 <lorenz_lehmann@yahoo.de>
2025-01-20 12:44:45 +0100

    refactor adapter_dune

--- backend/airflow/dags/metrics/metrics_dune.py
+++ backend/airflow/dags/metrics/metrics_dune.py
@@ -28,18 +28,34 @@ def etl():
     def run_fact_kpis():
         import os
         from src.db_connector import DbConnector
-        from src.adapters.adapter_dune import AdapterDune
+        from backend.src.adapters.adapter_dune import AdapterDune
 
         adapter_params = {
             'api_key' : os.getenv("DUNE_API")
         }
         load_params = {
-            'query_names' : None,
-            'days' : 'auto',
-            'load_type' : 'fact_kpis'
+            'queries': [
+                {
+                    'name': 'fundamentals',
+                    'query_id': 2607041,
+                    'params': {'days': 7}
+                },
+                {
+                    'name': 'stables_mcap',
+                    'query_id': 2608415,
+                    'params': {'days': 7}
+                },
+                {
+                    'name': 'economics_da',
+                    'query_id': 4046209,
+                    'params': {'days': 7}
+                }
+            ],
+            'prepare_df': 'prepare_df_metric_daily',
+            'load_type': 'fact_kpis'
         }
 
-       # initialize adapter
+        # initialize adapter
         db_connector = DbConnector()
         ad = AdapterDune(adapter_params, db_connector)
         # extract
@@ -51,18 +67,24 @@ def etl():
     def run_inscriptions():
         import os
         from src.db_connector import DbConnector
-        from src.adapters.adapter_dune import AdapterDune
+        from backend.src.adapters.adapter_dune import AdapterDune
 
         adapter_params = {
             'api_key' : os.getenv("DUNE_API")
         }
         load_params = {
-            'query_names' : None,
-            'days' : 1000,
-            'load_type' : 'inscriptions'
+            'queries': [
+                {
+                    'name': 'inscriptions',
+                    'query_id': 3346613,
+                    'params': {'days': 1000}
+                }
+            ],
+            'prepare_df': 'prepare_df_incriptions',
+            'load_type': 'inscription_addresses'
         }
 
-       # initialize adapter
+        # initialize adapter
         db_connector = DbConnector()
         ad = AdapterDune(adapter_params, db_connector)
         # extract
@@ -74,16 +96,23 @@ def etl():
     def run_glo_holders():
         import os
         from src.db_connector import DbConnector
-        from src.adapters.adapter_dune import AdapterDune
+        from backend.src.adapters.adapter_dune import AdapterDune
 
         adapter_params = {
             'api_key' : os.getenv("DUNE_API")
         }
         load_params = {
-            'load_type' : 'glo_holders'
+            'queries': [
+                {
+                    'name': 'glo_holders',
+                    'query_id': 3732844
+                }
+            ],
+            'prepare_df': 'prepare_df_glo_holders',
+            'load_type': 'glo_holders'
         }
 
-       # initialize adapter
+        # initialize adapter
         db_connector = DbConnector()
         ad = AdapterDune(adapter_params, db_connector)
         # extract
@@ -95,23 +124,29 @@ def etl():
     def check_for_depreciated_L2_trx():
         import os
         from src.db_connector import DbConnector
-        from src.adapters.adapter_dune import AdapterDune
+        from backend.src.adapters.adapter_dune import AdapterDune
         from src.misc.helper_functions import send_discord_message
-        
+
         adapter_params = {
             'api_key' : os.getenv("DUNE_API")
         }
         load_params = {
-            'load_type' : 'check-for-depreciated-L2-trx'
+            'queries': [
+                {
+                    'name': 'check-for-depreciated-L2-trx',
+                    'query_id': 4544157
+                }
+            ]
         }
         db_connector = DbConnector()
         ad = AdapterDune(adapter_params, db_connector)
         df = ad.extract(load_params)
         for i, row in df.iterrows():
             send_discord_message(f"<@790276642660548619> The economics mapping function for **{row.l2}** has changed. Details: settlement on {row.settlement_layer}, {row.no_of_trx} trx per day, from_address: {row.from_address}, to_address: {row.to_address}, method: {row.method}.", os.getenv('DISCORD_ALERTS'))
-    
+
+
     run_fact_kpis()
-    #run_inscriptions() # paused as of Jan 2025, noone uses inscriptions. Backfilling easily possible if needed
+    #run_inscriptions() # paused as of Jan 2025, no one uses inscriptions. Backfilling easily possible if needed.
     run_glo_holders()
     check_for_depreciated_L2_trx()
     
