6553841895523f27f7c8baf2c04e81dac276b885
naderbennour <naderbennour@outlook.com>
2025-01-27 10:43:46 +0100

    added Ink DAG

--- backend/src/adapters/rpc_funcs/utils.py
+++ backend/src/adapters/rpc_funcs/utils.py
@@ -478,7 +478,7 @@ def prep_dataframe_new(df, chain):
     Returns:
         pd.DataFrame: The prepared DataFrame with necessary columns, data types, and operations applied.
     """
-    op_chains = ['zora', 'base', 'optimism', 'gitcoin_pgn', 'mantle', 'mode', 'blast', 'redstone', 'orderly', 'derive', 'karak', 'ancient8', 'kroma', 'fraxtal', 'cyber', 'worldchain', 'mint']
+    op_chains = ['zora', 'base', 'optimism', 'gitcoin_pgn', 'mantle', 'mode', 'blast', 'redstone', 'orderly', 'derive', 'karak', 'ancient8', 'kroma', 'fraxtal', 'cyber', 'worldchain', 'mint', 'ink']
     default_chains = ['manta', 'metis']
     arbitrum_nitro_chains = ['arbitrum', 'gravity', 'real', 'arbitrum_nova']
     
