e311f5487524d76ebc23c47eb2fadd14ebbb6e21
Matthias Seidl <matthias@mseidl-analytics.de>
2025-04-01 10:17:19 +0200

    moved Celo maa, aa_30, aa_7 to own db

--- backend/src/queries/sql_queries.py
+++ backend/src/queries/sql_queries.py
@@ -186,6 +186,9 @@ sql_queries = [
         ,SQLQuery(metric_key = "txcosts_median_eth", origin_key = "gravity", jinja_path='chain_metrics/select_txcosts_median_custom_gas.sql.j2')
         
         ## Celo (op stack but CELO gas token and only data available start March 26th when L2 went live)
+        ,SQLQuery(metric_key = "maa", origin_key = "celo", jinja_path='chain_metrics/select_maa.sql.j2', currency_dependent = False)
+        ,SQLQuery(metric_key = "aa_last7d", origin_key = "celo", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2', query_parameters={"timerange" : 7}, currency_dependent = False)
+        ,SQLQuery(metric_key = "aa_last30d", origin_key = "celo", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2',query_parameters={"timerange" : 30}, currency_dependent = False)
         ,SQLQuery(metric_key = "cca_last7d_exclusive", origin_key = "celo", jinja_path='chain_metrics/select_cca_last7d.sql.j2', currency_dependent = False)
         ,SQLQuery(metric_key = "cca_weekly_exclusive", origin_key = "celo", jinja_path='chain_metrics/select_cca_weekly.sql.j2', currency_dependent = False)
 
